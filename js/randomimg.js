41bdac29e404119e30a37ae465921320b4f954fb
ManOnTheStreet <manonthestreet@tonx.com>
2016-12-11 11:39:16 -0500

    Added js random image on homepage, created js, changed structure for image paths

---
+++ js/randomimg.js
@@ -0,0 +1,12 @@
+<script type="text/javascript">
+// place your images in this array
+var random_images_array = ['durbar-square.jpg', 'lo-cai-Village-schoolgirl-sapa.jpg', 'bangkok-moto-taxi.jpg', 'bangkok-china-town-lion.jpg', 'sunset.jpg', 'sikkim-himalayas.jpg', 'nepal.jpg', 'wyoming.jpg'];
+
+function getRandomImage(imgAr, path) {
+    path = path || 'img/random/'; // default path here
+    var num = Math.floor( Math.random() * imgAr.length );
+    var img = imgAr[ num ];
+    var imgStr = '<img src="' + path + img + '" alt = "ManOnTheStreet.com" class="responsive-image">';
+    document.write(imgStr); document.close();
+}
+</script>
